da1e8935af4771bee2a34032a3eb0360b8d9aafc
AlexanderNBJ <alexandernbjunior@gmail.com>
2025-04-23 11:58:09 -0300

    Lista Semanal 08

---
+++ Listas Semanais/08 T1 Abril: Programação Dinâmica/1558.cpp
@@ -0,0 +1,31 @@
+/*
+    Aluno:  Alexander Neves Barbosa Junior
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    int num;
+    while(cin >> num){
+        bool achou = false;
+
+        for(int i=0; i*i <= num; i++){
+            int j = num - i*i;
+            int jsqrt = sqrt(j);
+
+            if(jsqrt*jsqrt == j){
+                achou = true;
+                break;
+            }
+        }
+        
+        if(achou)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
